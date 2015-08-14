71951a26167abce074f47417060a9950a773af21
ironholds <ironholds@gmail.com>
2015-08-14 10:02:38 -0400

    add a single-stage error handler.

--- main.R
+++ main.R
@@ -45,10 +45,25 @@ main <- function(){
   # Get the files
   files <- wmf::get_logfile(earliest = start_date, latest = end_date)
   
-  # Run this process over each file, distributing it over multiple cores so that
+  # Run the "memo" process over each file, distributing it over multiple cores so that
   # the end date is some time before the heat-death of the universe.
   results <- parallel::mclapply(X = files, FUN = memo, mc.preschedule = TRUE,
                                 mc.cores = 4, mc.cleanup = TRUE)
+  
+  # Perform a check for errors, and rerun (once!) if errors occurred, with half the cores
+  # in case it was a memory/connections issue.
+  result_classes <- unlist(lapply(results, function(x){return(class(x)[1])}))
+  errored_files <- files[which(result_classes == "try-error")]
+  if(length(errored_files)){
+    rerun_file_data <- parallel::mclapply(X = errored_files, FUN = memo, mc.preschedule = FALSE,
+                                          mc.cores = 2, mc.cleanup = TRUE)
+    test <- c(results, rerun_file_data)
+  }
+  
+  # Bind the results together into a single table and check that the date ranges are what we want.
+  # Because the log files switch over at 06:00 UTC, not 00:00 UTC, 1 January contains some of 31
+  # December, and some of 1 August is in the file for 2 August.
+  results <- do.call("rbind", results)
   results <- results[results$timestamp >= as.Date(start_date) & results$timestamp <= as.Date(end_date),]
   
 }
